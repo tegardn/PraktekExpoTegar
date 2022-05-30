9c66266dd6e3393411e14b990620e2cb017da74f
tegar dwi <dwitegar611@gmail.com>
2022-05-30 14:39:08 +0700

    Pertama

---
+++ router/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function Index() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Gallery" component={GalleryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/Home.js
@@ -0,0 +1,51 @@
+// import React from 'react';
+// import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+
+// const Separator = () => (
+//   <View style={styles.separator} />
+// );
+
+// export default function Home() {
+//   return (
+//     <View style={styles.container}>
+//       <NavigationContainer>
+//         <Button style={styles.profile}
+//         title="Profile"
+//         color="#f194ff"
+//         onPress={() => Alert.alert('Button with adjusted color pressed')}
+//       />
+
+//       <Separator/>
+
+//       <Button style={styles.gallery}
+//           title="Gallery"
+//           color="#f194ff"
+//           onPress={() => Alert.alert('Left button pressed')}
+//         />
+//       </NavigationContainer>
+      
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     textAlign: 'center',
+//     marginVertical: 8,
+//   },
+//   fixToText: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//   },
+//   separator: {
+//     marginVertical: 8,
+//     borderBottomColor: '#737373',
+//     borderBottomWidth: StyleSheet.hairlineWidth,
+//   },
+// });
